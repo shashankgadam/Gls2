7935964767908d8e3d3ffcf299956987e370952b
shash <shash@Shadow>
2024-10-05 20:36:30 +0530

    first commit

--- Gls2/src/main/java/demo1/gitdemo.java
+++ Gls2/src/main/java/demo1/gitdemo.java
@@ -27,7 +27,7 @@ public class gitdemo extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.print("Hey ");	}
+		System.out.print("Hey bsdk");	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
